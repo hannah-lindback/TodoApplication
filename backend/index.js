79bc6545cb9fca7031515048f7a57bcfa3673d0f
Hannah Lindbäck <hannahlindback@gmail.com>
2025-03-07 07:51:19 +0100

    added delete and put methods.

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,12 @@ app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
 });
 
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' });
     });
+
+// Basic operations for todos
 
 app.get('/api/todos', (req, res) => {
     res.json(todos);
@@ -21,5 +24,18 @@ app.get('/api/todos', (req, res) => {
 app.post('/api/todos', (req, res) => {
     const todo = req.body;
     todos.push(todo);
+    res.json(todo);
+    });
+
+app.delete('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    todos = todos.filter((todo, index) => index !== id);
+    res.json({ id });
+    });
+
+app.put('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const todo = req.body;
+    todos[id] = todo;
     res.json(todo);
     });
